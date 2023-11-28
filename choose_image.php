258ce25b9af9d8e0fdb89f5e922e6d54596fe150
azimeulker <azimeulker@hotmail.com>
2023-11-28 10:21:31 -0600

    image resize feature

--- choose_image.php
+++ choose_image.php
@@ -2,6 +2,45 @@
     $files = scandir('uploads');
     session_start();
     $_SESSION["image"] = "delete";
+
+
+    require ('ImageResize.php');
+    require ('ImageResizeException.php');
+    use \Gumlet\ImageResize;
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
+        $uploadDir = 'uploads/';
+        $uploadFile = $uploadDir . basename($_FILES['file']['name']);
+
+        // Check if the file is an allowed type (JPG, PNG, GIF, PDF)
+        $allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'application/pdf'];
+
+        if (in_array($_FILES['file']['type'], $allowedTypes)) {
+
+            if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadFile)) {
+                echo 'File is valid, and was successfully uploaded.';
+;
+                // Perform image resizing for supported image types
+                if ($_FILES['file']['type'] === 'image/jpeg' || $_FILES['file']['type'] === 'image/png' || $_FILES['file']['type'] === 'image/gif') {
+                
+                    $image = new ImageResize($uploadFile);
+                    $image->save('uploads/original_' . basename($_FILES['file']['name']));
+
+                    // Resize to 400px width
+                    $image->resizeToWidth(400);
+                    $image->save('uploads/medium_' . basename($_FILES['file']['name']));
+
+                    // Resize to 50px width
+                    $image->resizeToWidth(50);
+                    $image->save('uploads/thumbnail_' . basename($_FILES['file']['name']));
+                }
+            } else {
+                echo 'File upload failed.';
+            }
+        } else {
+            echo 'Invalid file type. Only JPG, PNG, GIF, and PDF files are allowed.';
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -67,9 +106,9 @@
                 <?php endforeach ?>
             </div>
 
-            <form action="fileUpload.php" method="post" enctype="multipart/form-data">
+            <form action="choose_image.php" method="post" enctype="multipart/form-data">
                 <h2>Upload your cover Image to the Gallery</h2>    
-                <input type="file" name="uploaded_file" id="choose_file">
+                <input type="file" name="file" id="file">
                 <div><input type="submit" name="submit" id="button1"  value="Upload Cover"></div>
             </form> 
         </section>
